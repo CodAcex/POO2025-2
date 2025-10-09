fbf9d5308bb1c4ba0e8b95f461be914e3e113791
CodAcex <carlosguenka@gmail.com>
2025-10-09 20:28:11 -0300

    Update Pagina.h

--- TRABALHO GA/Pagina.h
+++ TRABALHO GA/Pagina.h
@@ -6,29 +6,26 @@
 
 using namespace std;
 
-#define MAX_FIGURINHAS_PAGINA 10 // Capacidade de figurinhas por página
+#define MAX_FIGURINHAS_PAGINA 10 
 
 class Pagina {
 private:
     string titulo;
     int nroInicial;
     int nroFinal;
-    Figurinha figurinhasNaPagina[MAX_FIGURINHAS_PAGINA]; // Figurinhas coladas
+    Figurinha figurinhasNaPagina[MAX_FIGURINHAS_PAGINA];
     int nroFigurinhas;
 
 public:
-    // Construtor
+    
     Pagina();
     Pagina(string titulo, int nroInicial, int nroFinal);
     ~Pagina();
     
-    // NOVO: Getters que o Album precisa (Sem const)
     int getNroInicial();
     int getNroFinal();
-    
-    // Métodos
     void adicionarFigurinha(Figurinha f);
     void listar();
 };
 
-#endif // PAGINA_H
+#endif

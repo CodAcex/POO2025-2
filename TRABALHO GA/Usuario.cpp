dd333a3643e854f42bbd0ac6a359900c07ec63de
CodAcex <carlosguenka@gmail.com>
2025-10-09 20:30:18 -0300

    Update Usuario.cpp

--- TRABALHO GA/Usuario.cpp
+++ TRABALHO GA/Usuario.cpp
@@ -5,14 +5,12 @@ using namespace std;
 void Usuario::cadastrar(string nome, string senha) {
     this->nomeDeUsuario = nome;
     this->senha = senha;
-    // meuAlbum é inicializado automaticamente pelo construtor padrão
 }
 
 bool Usuario::verificarLogin(string nome, string senha) {
     return (this->nomeDeUsuario == nome && this->senha == senha);
 }
 
-// Getters (Sem const)
 string Usuario::getNomeDeUsuario() { return this->nomeDeUsuario; }
 string Usuario::getSenha() { return this->senha; }
 Album& Usuario::getAlbum() { return this->meuAlbum; }
